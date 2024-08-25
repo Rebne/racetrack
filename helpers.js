311a3410724aa69082000e7cb4438d8663ab0584
Rebne <renehgren@gmail.com>
2024-08-25 21:31:08 +0300

    Did not include the helpers file with last commit

---
+++ helpers.js
@@ -0,0 +1,14 @@
+function min(x, y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+
+function isStringNumber(str) {
+    const num = Number(str);
+    return str === String(num);
+}
+
